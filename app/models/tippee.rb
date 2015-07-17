a8762c3f62d0a0810d4b681295e6bbdd4678b227
Kelly Munro <kellymdev@gmail.com>
2015-07-17 19:38:06 +1200

    Add tippee token to tippee model and a before_create callback to generate the token

--- app/models/tippee.rb
+++ app/models/tippee.rb
@@ -1,4 +1,10 @@
 class Tippee < ActiveRecord::Base
   belongs_to :user
   has_many :tips
+
+  before_create :generate_tippee_token
+
+  def generate_tippee_token
+    self.tippee_token = SecureRandom.urlsafe_base64
+  end
 end
