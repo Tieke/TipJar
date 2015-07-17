d4bf93403e1d3fe2b8aff85138787c25f584eb1f
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-17 13:56:52 +1200

    wrote three tests and added sequence to user factory

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+	describe "#index" do
+		before do
+			5.times { create(:user) }
+			get :index
+		end
+
+		it { should respond_with(200) }
+
+		it "should expect the body of the last response to be all the users as json" do
+			expect(response.body).to eq(User.all.to_json)
+		end
+		
+		it "should assign @users to all Users in the DB" do
+			expect(assigns(:users)).to eq(User.all)
+		end
+
+	end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    username "freddythefrog"
+    sequence(:username) { |n| "freddythefrog#{n}" }
     full_name "Freddy the Frog"
     terms_version "0.1"
     terms_date Time.now
     admin false
     image_url "http://www.firstgroup.com/ukbus/assets/images/midlands/freddie.jpg"
     balance 10
-    email "freddy@example.com"
+    sequence(:email) { |n| "freddy#{n}@example.com" }
     password "test1234"
   end
 end
