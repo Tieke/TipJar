57a83f92ead47dc656ec93bb5f81da3402404c8e
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-17 13:32:37 +1200

    working on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
   root to: "home#index"
+
+  resources :users, except: [:new, :create] do
+  	resources :tips, except: [:edit, :update, :destroy]
+  end
+
+  get 'users/:user_id/tips/given', to: 'tips#given'
+
+  get 'users/:user_id/tips/received', to: 'tips#received'
+
+
+ 
+
+
+
+
+  # transactions
+  # 	create
+  # 		#new
+  # 		#create
+  # 	read
+  # 		#show
+  # 		#index
+  # 	update
+  # 		#edit
+  # 		#update
+  # 	delete
+  # 		#delete
+  # 		#desroy  
+  # follows
+  # 	create
+  # 		#new
+  # 		#create
+  # 	read
+  # 		#show
+  # 		#index
+  # 	update
+  # 		#edit
+  # 		#update
+  # 	delete
+  # 		#delete
+  # 		#desroy
 end
