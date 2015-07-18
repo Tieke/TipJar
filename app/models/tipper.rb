d89621ad275f4d9340ce9a75dcd7da30e46efdf0
rhyseda <rhys_eda@portegys.org>
2015-07-18 14:33:17 +1200

    Added code and tests, updated fatory girl and seeds file

--- app/models/tipper.rb
+++ app/models/tipper.rb
@@ -1,4 +1,6 @@
 class Tipper < ActiveRecord::Base
   belongs_to :user
   has_many :tips
+
+  validates :standard_tip_amount, presence: true
 end
--- spec/models/tipper_spec.rb
+++ spec/models/tipper_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Tipper, type: :model do
+  let!(:tipper) { create(:tipper) }
   describe "relationships" do
     it { should belong_to(:user) }
     it { should have_many(:tips) }
+  end
+
+  describe "validations" do
+    it "has standard_tip_amount" do
+      expect(tipper).to validate_presence_of(:standard_tip_amount)
+    end
+  end
+
+  after do
+    Tipper.destroy_all
   end
 end
