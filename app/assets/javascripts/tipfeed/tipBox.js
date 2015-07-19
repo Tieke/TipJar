02a586aa5cebcf48db7802090e8c9322b83428f4
Elisabeth McKoy <elisabeth.mckoy@gmail.com>
2015-07-19 12:21:05 +1200

    set up, linking files

---
+++ app/assets/javascripts/tipfeed/tipBox.js
@@ -0,0 +1,9 @@
+var TipBox = React.createClass({displayName: 'TipBox',
+  render: function() {
+    return (
+      React.createElement('div', {className: "TipBox"},
+        "Hello, world! I am a TipBox."
+      )         
+    );
+  }
+});
