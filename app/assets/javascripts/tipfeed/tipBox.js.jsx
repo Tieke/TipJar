525613f94973fed97453082f3a1a540f7fe2a2c5
Elisabeth McKoy <elisabeth.mckoy@gmail.com>
2015-07-19 15:29:18 +1200

    fixing controller index

--- app/assets/javascripts/tipfeed/tipBox.js.jsx
+++ app/assets/javascripts/tipfeed/tipBox.js.jsx
@@ -1,16 +1,57 @@
-$(document).ready(function() {
-	var TipBox = React.createClass({displayName: 'TipBox',
-	  render: function() {
-	    return (
-	    	<div className="tipBox">
-	    		<h1>things</h1>
-	    	</div>        
-	    );
-	  }
-	});
-
-	React.render(
-	  React.createElement(TipBox, null),
-	  document.getElementById('main_body')
-	);
+var TipBox = React.createClass({
+  loadTipsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadTipsFromServer();
+    setInterval(this.loadTipsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="tipBox">
+        <TipCreator data={this.state.data} />
+      </div>
+    );
+  }
 });
+
+
+
+
+{
+	tips: [tipObjects......],
+	givers: []
+}
+
+[
+	{
+		tips: tipObject,
+		giver: giverObject,
+		receiver: receiverObject
+	},
+	{
+
+	},
+]
+
+
+
+
+
+
+
+
+
