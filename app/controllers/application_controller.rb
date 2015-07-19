378e74c47d5c315a1010187a1210634f7630c739
Kelly Munro <kellymdev@gmail.com>
2015-07-19 13:33:27 +1200

    Devise login form added to Welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
   protected
   def set_user_params
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:full_name, :username, :terms_version, :terms_date, :admin, :image_url, :balance, :email, :password) }
