1faeb61fc8697d421ac9c86ffc45e5e0c8cc2ab2
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-21 16:11:42 +1200

    changes from rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	# before_action :authenticate_user!
+	before_action :authenticate_user!
 
 	def index
 		@users = User.all
@@ -16,14 +16,7 @@ class UsersController < ApplicationController
 		@withdrawals = @user.withdrawals
 		@deposits = @user.deposits
 		render json: { "withdrawals" => @withdrawals, "deposits" => @deposits }
-	end
-
-	def initiate
-
-	end
-
-	def retrieve_checkout 
-
+		
 	end
 
 	def topup
@@ -31,9 +24,21 @@ class UsersController < ApplicationController
   	p client = BitPayClient.last
   	p invoice = client.create_invoice(price: "5.00", currency: "USD", facade: "merchant")
   	@invoice_url = invoice["url"]
+	#background job starts
+		#check invoice["status"] every 2 minutes until status == paid || pending
+			#if it us, update balance with amount converted to standarized currency
+		#else 
+			#balance update pending page
 	end
 
-  def transfer
+  def withdraw
+  	@user = current_user
+  	client = BitPayClient.last
+  	payout_response = HTTParty.post("https://test.bitpay.com/payouts", query: {'amount'=>1.00,'currency'=>'USD','account'=>'TewzSJHWriVowxFYgAsa1TrZzkxnegawhcc', 'address'=>'n4JvFCnA1WRFiEreWnLLLm8xrsMzRmL6hZ', 'instructions'=>['test instructions'], 'effectiveDate'=>"#{Date.today}"})
+
+		# payout_response = HTTParty.post("https://test.bitpay.com/payouts", query: {'amount'=>params[:amount],'currency'=>params[:currency],'account'=>'TewzSJHWriVowxFYgAsa1TrZzkxnegawhcc', 'address'=>params[:address], 'facade'=>'Merchant', 'instructions'=>[], 'effectiveDate'=>Date.today})
   end
 
 end
+
+HTTParty.post("test.bitpay.com/tokens", query: {'label'=>'TipJar', 'id'=>'TewzSJHWriVowxFYgAsa1TrZzkxnegawhcc', 'pairingCode'=>'bpsPdGQ'})
