41e7450f78ef0ad64633e549c65b56f9478d9b0e
Elisabeth McKoy <elisabeth.mckoy@gmail.com>
2015-07-22 11:55:23 +1200

    styling and started following feed

--- app/assets/javascripts/profile-page/scripts/followers.js.jsx
+++ app/assets/javascripts/profile-page/scripts/followers.js.jsx
@@ -1,8 +1,11 @@
 var Followers = React.createClass({
+	// ajax for getting user objects
+	// user id, image, name 
 	render: function() {
 		return (
 			<div className="followers col-lg-3">
 				<p>followers</p>
+				// 9 random profile images from users follow you
 			</div>
 		)
 	}
--- app/assets/javascripts/profile-page/scripts/following.js.jsx
+++ app/assets/javascripts/profile-page/scripts/following.js.jsx
@@ -1,8 +1,11 @@
 var Following = React.createClass({
+	// ajax for getting user objects
+	// user id, image, name
 	render: function() {
 		return (
 			<div className="following col-lg-3">
 				<p>Following</p>
+				// 9 random profile images from users you follow
 			</div>
 		)
 	}
