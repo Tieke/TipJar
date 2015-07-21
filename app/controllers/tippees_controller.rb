540be9a9dacaabd485d649f8123d6cceef0dd381
Kelly Munro <kellymdev@gmail.com>
2015-07-21 14:25:27 +1200

    Remove unused tippees_controller routes and views

--- app/controllers/tippees_controller.rb
+++ app/controllers/tippees_controller.rb
@@ -1,21 +1,6 @@
 class TippeesController < ApplicationController
   before_action :authenticate_user!
 
-  # def token
-  #   if !current_user.tippee
-  #     @tippee = Tippee.new(user_id: current_user.id)
-  #     render :new
-  #   else
-  #     @tippee_token = current_user.tippee.tippee_token
-  #     @tipUrl = "http://localhost:3000/tips/create/"
-  #     render :token
-  #   end
-  # end
-
-  # def new
-  #   @tippee = Tippee.new
-  # end
-
   def create
     @tippee = Tippee.create(user_id: current_user.id)
     redirect_to "/users/#{current_user.id}/tip_settings"
