1a56996e4ef59075bfa0b6eaeda37a91f81fc67f
rhyseda <rhys_eda@portegys.org>
2015-07-22 12:13:28 +1200

    Commit after merge origin/master

---
+++ Script.rb
@@ -0,0 +1,53 @@
+Cast of Characters:
+mC
+testress
+the_Cool_Collected_One
+gitMeister
+acting_is_reacting
+big_Picture_Guy
+
+
+mC: 'Welcome friends and family. Allow me to introduce team Tieke. We hope the passion, time, and fun that was put into this projec will be communicated. Sometimes actions speak louder than words, so without furher ado, let me present to you TipJar.'
+
+	- app is launched and breaks, to a full 5 seconds of awkward silence -
+
+gitMeister: 'did you remember to pull down from the master branch?'
+
+testress: 'run the specs and we"ll debug them one at a time'
+
+	- run rspec, watch wall of red -
+
+big_Picture_Guy:  'thatll take too long. Try restarting the heroku dynamos'
+
+the_Cool_Collected_One: 'maybe theres a stack overflow'
+
+	- everyone flips open a laptop and starts searching - 
+
+mC: 'Sorry everyone. Rohan youll edit this part out, right?' 
+
+	- forced laugh -
+
+acting_is_reacting: 'did you remember to document.ready in applicaion.js?'
+
+gitMeister: 'try to truncate and reseed the database'
+
+the_Cool_Collected_One: 'Maybe we should ask one of the coaches for some assistance?'
+
+acting_is_reacting: 'we cant do that now. lets just run it in local host'
+
+big_Picture_Guy: 'maybe I should show everyone some 3D drones in the meantime?'
+
+testress: 'I think I found something...'
+
+	- some coding and then app works -
+
+mC: 'Thanks for being patient everyone. On with the show.'
+
+acting_is_reacting: 'Look at that link, we should click on it and thank him before we forget'
+
+	- presentation begins - 
+
+
+
+
+
