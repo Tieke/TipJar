c97bea567f9a5222abe514ea60155562cc11d355
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-22 23:24:04 +1200

    kyle_quick_changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721202654) do
+ActiveRecord::Schema.define(version: 20150722110149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,6 @@ ActiveRecord::Schema.define(version: 20150721202654) do
     t.datetime "updated_at",   null: false
     t.integer  "followed_id"
   end
-
-  add_index "follows", ["following_id"], name: "index_follows_on_following_id", using: :btree
 
   create_table "tippees", force: :cascade do |t|
     t.integer  "user_id"
@@ -70,24 +68,24 @@ ActiveRecord::Schema.define(version: 20150721202654) do
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
-    t.string   "username",                             null: false
+    t.string   "username",                                                                                                           null: false
     t.string   "terms_version"
     t.datetime "terms_date"
     t.boolean  "admin"
-    t.string   "image_url"
+    t.string   "image_url",              default: "https://s3-ap-southeast-2.amazonaws.com/tipjarwellington/images/tipjar-logo.png"
     t.decimal  "balance",                default: 0.0
-    t.string   "email",                  default: "",  null: false
-    t.string   "encrypted_password",     default: "",  null: false
+    t.string   "email",                  default: "",                                                                                null: false
+    t.string   "encrypted_password",     default: "",                                                                                null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,   null: false
+    t.integer  "sign_in_count",          default: 0,                                                                                 null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                                                                                                         null: false
+    t.datetime "updated_at",                                                                                                         null: false
     t.string   "coinbase_access_token"
     t.string   "coinbase_user_id"
     t.text     "about"
