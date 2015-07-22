31cd5779eaef8e7b9881074d58320cb8977b4787
rhyseda <rhys_eda@portegys.org>
2015-07-22 14:33:46 +1200

    added team squad user profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,17 +63,17 @@ tipper_array = Tipper.pluck(:id)
 random_urls = [
   "https://www.youtube.com/watch?v=cDaepLDC0oE",
   "https://www.youtube.com/watch?v=Xs5IMWjI1SA",
-  "https://music.quietentertainer.com/track/walkie-talkie-irn-mnky-beat-down-mix",
+  # "https://music.quietentertainer.com/track/walkie-talkie-irn-mnky-beat-down-mix",
   "https://www.youtube.com/watch?v=uR_8JuIXvpA",
   "https://www.youtube.com/watch?v=_T2XcE9UhWI",
   "https://www.pinterest.com/pin/53832158023475798/",
   "https://www.pinterest.com/pin/269301252694607860/",
   "https://www.pinterest.com/pin/546483736011610386/",
   "https://www.pinterest.com/pin/301811612506221942/",
-  "https://www.stuff.co.nz/national/quizzes/daily-quizzes/12236967/Afternoon-trivia-quiz-July-20",
+  # "https://www.stuff.co.nz/national/quizzes/daily-quizzes/12236967/Afternoon-trivia-quiz-July-20",
   "https://djshadow.bandcamp.com/releases/",
-  "https://www.stuff.co.nz/national/quizzes/12230614/Daily-trivia-quiz-July-20",
-  "https://www.stuff.co.nz/auckland/local-news/manukau-courier/69876532/unusual-baggage-seized-at-border",
+  # "https://www.stuff.co.nz/national/quizzes/12230614/Daily-trivia-quiz-July-20",
+  # "https://www.stuff.co.nz/auckland/local-news/manukau-courier/69876532/unusual-baggage-seized-at-border",
   "https://soundcloud.com/beatkingkong-clubgod-amen/going-down-beatking-dirty",
   "https://soundcloud.com/beatteamsupreme/jnthn-stein-true",
   "https://soundcloud.com/sandpeoplecologne/sandpeople-x-made-in-m-bypassed",
@@ -81,7 +81,11 @@ random_urls = [
   "https://stackoverflow.com/questions/31511195/import-scope-and-specify-path",
   "https://stackoverflow.com/questions/31511196/does-web-scraping-have-patterns",
   "https://stackoverflow.com/questions/31511112/c-sharp-named-pipes-in-message-mode-sometimes-merge-messages",
-  "https://stackoverflow.com/questions/31511200/looking-for-a-daydeal-plugin-with-custom-sorting-functionality"
+  "https://stackoverflow.com/questions/31511200/looking-for-a-daydeal-plugin-with-custom-sorting-functionality",
+  "http://www.expressnews.com/news/education/article/Texas-textbook-standards-on-Civil-War-concern-6377518.php",
+  "http://www.ctvnews.ca/canada/man-who-survived-lightning-strike-wins-1m-jackpot-with-co-worker-1.2478542#",
+  "http://www.cbc.ca/passionateeye/episodes//apples-broken-promises",
+  "https://www.minds.com/blog/view/468466376596533248/new-co2-recycler-captures-carbon-dioxide-from-the-atmosphere-and-turns-it-back-into-fuel"
 ].shuffle
 
 
@@ -98,6 +102,44 @@ random_urls = [
   )
 end
 
+# Our stuff after here
 
 
 
+
+
+User.create(full_name: "Te Aihe Butler", email: "teaihe@team.squad", username: "Te",
+  password: "qwerty123",
+  image_url: "https://avatars3.githubusercontent.com/u/9012942?v=3&s=460",
+  balance: rand(3..10),
+  terms_version: "1.0")
+
+User.create(full_name: "Liz McKoy", email: "liz@team.squad", username: "Liz",
+  password: "qwerty123",
+  image_url: "https://avatars0.githubusercontent.com/u/11071977?v=3&s=460",
+  balance: rand(3..10),
+  terms_version: "1.0")
+
+User.create(full_name: "Kyle Black Snow", email: "kyle@team.squad", username: "Kyle",
+  password: "qwerty123",
+  image_url: "https://avatars2.githubusercontent.com/u/11069001?v=3&s=460",
+  balance: rand(3..10),
+  terms_version: "1.0")
+
+User.create(full_name: "Kelly Munro", email: "kelly@team.squad", username: "Kelly",
+  password: "qwerty123",
+  image_url: "https://avatars2.githubusercontent.com/u/11514966?v=3&s=460",
+  balance: rand(3..10),
+  terms_version: "1.0")
+
+User.create(full_name: "Will Sklenars", email: "will@team.squad", username: "Will",
+  password: "qwerty123",
+  image_url: "https://avatars2.githubusercontent.com/u/11266273?v=3&s=460",
+  balance: rand(3..10),
+  terms_version: "1.0")
+
+User.create(full_name: "Rhys Portegys", email: "rhys@team.squad", username: "Rhys",
+  password: "qwerty123",
+  image_url: "https://avatars0.githubusercontent.com/u/11514884?v=3&s=460",
+  balance: rand(3..10),
+  terms_version: "1.0")
